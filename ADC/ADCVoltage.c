a018181c3cf065d11608f63985c22acc102db680
mholodniuk <259228@student.pwr.edu.pl>
2022-05-20 11:54:58 +0200

    adc

--- ADC/ADCVoltage.c
+++ ADC/ADCVoltage.c
@@ -10,31 +10,29 @@ volatile char znaki[4] = { ~(0b01110111), ~(0b01111100), ~(0b00111001), ~(0b0101
 char cyfra[10] = { ~(0b00111111), ~(0b00000110), ~(0b01011011), ~(0b01001111), ~(0b01100110), ~(0b01101101),
                    ~(0b01111101), ~(0b00000111), ~(0b01111111), ~(0b01101111)};
 
-char min[10] = { ~(0b10111111), ~(0b10000110), ~(0b11011011), ~(0b11001111), ~(0b11100110), ~(0b11101101),
-                   ~(0b11111101), ~(0b10000111), ~(0b11111111), ~(0b11101111)};
+char min[10] = { ~(0b0111111), ~(0b0000110), ~(0b1011011), ~(0b1001111), ~(0b1100110), ~(0b1101101),
+                   ~(0b1111101), ~(0b0000111), ~(0b1111111), ~(0b1101111)};
 
 
 
 void ADC_init() {
     // konfiguracja napięcia referencyjnego – wybrać napięcie AVCC
-    cbi(ADMUX, REFS1);
-    sbi(ADMUX, REFS0);
+	sbi(ADMUX, REFS0);
+		cbi(ADMUX, REFS1);
 
-    // konfiguracja podzielnika częstotliwości dla układu przetwornika (częstotliwość sygnału taktującego)
-    // 8M / DivisionFactor
-    sbi(ADCSRA, ADPS0);
-    sbi(ADCSRA, ADPS1);
-    sbi(ADCSRA, ADPS2);
+	//	Czestotliwosc sygnalu taktujacego 62,5 kHz
+		sbi(ADCSRA, ADPS0);
+		sbi(ADCSRA, ADPS1);
+		sbi(ADCSRA, ADPS2);
 
-    // uruchomienie układu przetwornika
-    sbi(ADCSRA, ADEN);
+		sbi(ADCSRA, ADEN); //Wlaczenie przetwornika
 
-    // konfiguracja | wybór kanału/pinu na którym będzie dokonywany pomiar
-    cbi(ADMUX, MUX0);
-    cbi(ADMUX, MUX1);
-    cbi(ADMUX, MUX2);
-    cbi(ADMUX, MUX3);
-    cbi(ADMUX, MUX4);
+	//	Konfiguracja ustawia ADC do pomiaru
+		cbi(ADMUX, MUX0);
+		cbi(ADMUX, MUX1);
+		cbi(ADMUX, MUX2);
+		cbi(ADMUX, MUX3);
+		cbi(ADMUX, MUX4);
 }
 
 uint16_t ADC_10bit() {
@@ -44,9 +42,8 @@ uint16_t ADC_10bit() {
 }
 
 
-int ConvertToVoltage(uint16_t ADC) {
-    return (5 * ADC / 1023) * 100;
-
+uint16_t ConvertToVoltage(uint16_t  adc) {
+    return ((5.00 * adc / 1023.00) * 100.00);
 }
 
 
@@ -57,13 +54,14 @@ void TimerInit() {
   sbi(TCCR1B, WGM12);
   cbi(TCCR1A, WGM11);
   cbi(TCCR1A, WGM10);
+
   //Wybranie dzielnika czestotliwosci
   sbi(TCCR1B, CS10);
   cbi(TCCR1B, CS11);
   sbi(TCCR1B, CS12);
-  
-  //Zapisanie do OCR1A wartosci odpowiadajacej 0,5s
-  OCR1A = 3906/2;
+
+  // 100Hz
+  OCR1A = 78;
   //Uruchomienie przerwania OCIE1A
   sbi(TIMSK, OCIE1A);
 }
@@ -86,7 +84,7 @@ void seg7Init() {
 
 
 void seg7ShowCyfra(uint8_t cyfraDoWyswietlenia) {			//Wyswietla na wyswietlaczu 7 segmentowym cyfre z argumentu
-  PORTD = cyfra[cyfraDoWyswietlenia];   
+  PORTD = cyfra[cyfraDoWyswietlenia];
 }
 
 void seg7ShowMin(uint8_t cyfraDoWyswietlenia) {
@@ -97,104 +95,56 @@ void seg7ShowMin(uint8_t cyfraDoWyswietlenia) {
 void seg7Show4Cyfry(uint16_t liczba) {
   PORTC = 0b1110;
       seg7ShowCyfra(liczba%10);
-     // _delay_ms(1);
+      _delay_ms(2);
       if (liczba > 9)
       {
         PORTC = 0b1101;
         seg7ShowCyfra((liczba % 100) / 10);
       }
-    //  _delay_ms(1);
+      _delay_ms(2);
       if (liczba > 99)
       {
         PORTC = 0b1011;
         seg7ShowCyfra((liczba % 1000) / 100);
       }
-    //  _delay_ms(1);
+      _delay_ms(2);
       if (liczba > 999)
       {
         PORTC = 0b0111;
         seg7ShowCyfra((liczba % 10000) / 1000);
       }
-     // _delay_ms(1);
+      _delay_ms(2);
       if (liczba > 9999)
       {
         liczba = 0;
         PORTC = 0b1110;
         seg7ShowCyfra(liczba);
       }
-      _delay_ms(1);
+      //_delay_ms(2);
 }
 
-void seg7zegarek(uint16_t liczba) {
-  PORTC = 0b1110;
-      seg7ShowCyfra(liczba%10);
 
-      if (liczba > 9)
-      {
-        PORTC = 0b1101;
-        seg7ShowCyfra((liczba%60) / 10);
-      }
+volatile int pomiar;
 
-      if (liczba > 59)
-      {
-        PORTC = 0b1011;
-        seg7ShowMin((liczba % 600) / 60);
-      }
-
-      if (liczba > 599)
-      {
-        PORTC = 0b0111;
-        seg7ShowCyfra((liczba % 6000) / 600);
-      }
-
-      if (liczba > 5999)
-      {
-        liczba = 0;
-        PORTC = 0b1110;
-        seg7ShowCyfra(liczba);
-      }
-
-}
-
-//Funkcja wyswietla na wyswietlaczu litery, cyfry z tablicy znaki
-/*      zadanie na ocenę 5.5              */
-void seg7Show4Litery(uint8_t cyfraDoWyswietlenia) {
-  PORTD = znaki[cyfraDoWyswietlenia];
-}
 
 int main() {
   minuty = 0;
   sekundy = 0;
   TimerInit();
   seg7Init();
+  ADC_init();
   licz=0;
 
   sei();  //funkcja uruchamia globalne przerwania
 
 
-
-  // Wyświetlanie 1 cyfry
-//      PORTC = 0x0;
-//      while (1) {
-//        seg7ShowCyfra(0);
-//        _delay_ms(100);
-//      }
-
-  //  // Wyświetlanie 4 cyfr
-//      while (1) {
-//      seg7zegarek(licz);
-//      }
-
-// Wyświetlanie Liter
-//      while(1)
-//      {
-//        seg7Show4Litery(1);
-//      }
-
       while(1)
       {
-        //seg7Show4Cyfry(ConvertToVoltage(ADC_10bit()));
-        _delay_ms(2000);
+			pomiar = ConvertToVoltage(ADC_10bit());
+    	  //seg7Show4Cyfry(ConvertToVoltage(ADC_10bit()));
+    	  //seg7Show4Cyfry(ConvertToVoltage(500));
+    	  // seg7Show4Cyfry(2137);
+        _delay_ms(100);
       }
 
   return 0;
@@ -202,5 +152,6 @@ int main() {
 
 //Funkcja uruchamiana z przerwaniem po przepelnieniu licznika w timer1
 ISR(TIMER1_COMPA_vect) {
-  // seg7Show4Cyfry(ConvertToVoltage(ADC_10bit()));
+	//seg7Show4Cyfry(ConvertToVoltage(ADC_10bit()));
+	seg7Show4Cyfry(pomiar);
 }
